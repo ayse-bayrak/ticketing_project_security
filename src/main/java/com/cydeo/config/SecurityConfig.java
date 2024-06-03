555ab18ff84f7438bb5f4ff74b6f52c17137ac3e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-03 14:51:31 +0300

    logout stuff is added, in SecurityConfig class and header.html

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -10,6 +10,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -26,34 +28,44 @@ public class SecurityConfig {
 //        userList.add(
 //                new User("ayse", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
 //    return new InMemoryUserDetailsManager(userList);
-//    }
+//    } FIRSTLY  I CREATE AS A MANUALLY
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       // I use this SecurityFilterChain interface because I want to introduce my own validation from to Spring
         return http
                 .authorizeRequests()
-               // .antMatchers("/user/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasAuthority("Admin")
+               // .antMatchers("/user/**").hasRole("ADMIN") // ROLE_ADMIN--> if I want to hasRole I need to go to my database and I need to change the Role_..
+                .antMatchers("/user/**").hasAuthority("Admin") // in my app, certain roles should able to see ceratin pages
                 .antMatchers("/project/**").hasAuthority("Manager")
                 .antMatchers("/task/employee/**").hasAuthority("Employee")
                 .antMatchers("/task/**").hasAuthority("Manager")
-               // .antMatchers("task/**").hasAnyRole("EMPLOYEE", "ADMIN") // DEFINE MORE THAN ONE ROLE
-               // .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")   // hasAuthority needs to be ROLE_
-                .antMatchers(
+               // .antMatchers("task/**").hasAnyRole("EMPLOYEE", "ADMIN") // I can DEFINE MORE THAN ONE ROLE , DEPENDS BUSINESS LOGIC, now only example we dont have more than roles
+               // .antMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")   // hasAuthority needs to be ROLE_,
+                .antMatchers( // antMatchers related with the pages
                         "/",
                          "login",
                         "/fragments/**",
                         "/assets/**",
                         "/images/**"
-                ).permitAll() // anybody can access
+                ).permitAll() // everybody should be able to access to this pages, anybody can access
                 .anyRequest().authenticated()
                 .and()
                 //.httpBasic()// I want to use my own login page so I command it
-                .formLogin()
+                .formLogin() // I want to introduce my own validation form to Spring
                      .loginPage("/login")
-                     .defaultSuccessUrl("/welcome")
-                     .failureUrl("/login?error=true")
-                     .permitAll()
-                .and().build();
+                     .defaultSuccessUrl("/welcome")// whenever User is successfull done or User autoticated correct username and password this is the page we gonna land it,  it is gonna navigate to welcome page
+                     .failureUrl("/login?error=true") // if user put the wrong information username and password, I want to navigate to this URL
+                     .permitAll() // should be accessible by anyone
+                .and() // we put between this separate part .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .and()
+                .rememberMe()
+                .tokenValiditySeconds(120)
+                .key("cydeo")
+                .userDetailsService(securityService)
+                .and().build(); // at the end finish everything we put .build()
     }
 }
