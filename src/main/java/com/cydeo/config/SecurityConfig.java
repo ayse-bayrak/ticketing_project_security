cfc5d7a56916d1a4573c5fa2e536488d77688278
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-04 14:12:36 +0300

    rememberMe is added, in SecurityConfig class

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -1,24 +1,21 @@
 package com.cydeo.config;
 
+import com.cydeo.service.SecurityService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AndRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
+
+    private final SecurityService securityService;
+
+    public SecurityConfig(SecurityService securityService) {
+        this.securityService = securityService;
+    }
+
 
 //    @Bean
 //    public UserDetailsService userDetailService (PasswordEncoder encoder) {
@@ -55,7 +52,8 @@ public class SecurityConfig {
                 .formLogin() // I want to introduce my own validation form to Spring
                      .loginPage("/login")
                      .defaultSuccessUrl("/welcome")// whenever User is successfull done or User autoticated correct username and password this is the page we gonna land it,  it is gonna navigate to welcome page
-                     .failureUrl("/login?error=true") // if user put the wrong information username and password, I want to navigate to this URL
+
+                .failureUrl("/login?error=true") // if user put the wrong information username and password, I want to navigate to this URL
                      .permitAll() // should be accessible by anyone
                 .and() // we put between this separate part .and()
                 .logout()
