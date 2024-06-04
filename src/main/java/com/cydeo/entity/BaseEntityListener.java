38ff6060675ae8473478758b9fb7301ff5c1c82b
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-04 22:28:29 +0300

    BaseEntityListener is added

--- src/main/java/com/cydeo/entity/BaseEntityListener.java
+++ src/main/java/com/cydeo/entity/BaseEntityListener.java
@@ -1,5 +1,6 @@
 package com.cydeo.entity;
 
+import com.cydeo.entity.common.UserPrincipal;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import java.time.LocalDateTime;
 
 @Component
 public class BaseEntityListener extends AuditingEntityListener {
@@ -17,16 +19,27 @@ public class BaseEntityListener extends AuditingEntityListener {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 //I am getting the all information belongs to user who login in the system
 
-        this.insertDateTime = LocalDateTime.now();
-        this.lastUpdateDateTime=LocalDateTime.now();
-        this.insertUserId=1L;
-        this.lastUpdateUserId=1L;
+        baseEntity.setInsertDateTime(LocalDateTime.now());
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+
+      if (authentication != null && authentication.getName().equals("anonymousUser")) {
+          Object principal = authentication.getPrincipal();
+          baseEntity.setInsertUserId(((UserPrincipal) principal) .getId());
+          baseEntity.setLastUpdateUserId( ((UserPrincipal) principal) .getId());
+      }
     }
 
     @PreUpdate
-    private void onPreUpdate(){
-        this.lastUpdateDateTime=LocalDateTime.now();
-        this.lastUpdateUserId=1L;
+    private void onPreUpdate(BaseEntity baseEntity){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        baseEntity.setLastUpdateDateTime(LocalDateTime.now());
+        if (authentication != null && authentication.getName().equals("anonymousUser")) {
+            Object principal = authentication.getPrincipal();
+            baseEntity.setInsertUserId(((UserPrincipal) principal) .getId());
+            baseEntity.setLastUpdateUserId( ((UserPrincipal) principal) .getId());
+        }
     }
+
+   //whenever we create or update something, we are keeping some information which is who did it, what time did it?
 
 }
