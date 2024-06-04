0de803c347d74bdc19c9feee5289ff4800a3193e
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-04 14:14:52 +0300

    some changes

--- src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -21,12 +21,13 @@ public class SecurityServiceImpl implements SecurityService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user =userRepository.findByUserNameAndIsDeleted(username, false);
+        User user =userRepository.findByUserNameAndIsDeleted(username, false); // get to user from the database that needs to be validate
         if (user==null) {
             throw new UsernameNotFoundException(username);
         }
 
         return new UserPrincipal(user); // for mapper convert to UserDetails object which is understand Spring
         //get the user from db,and convert to user springs understands by using userPrincipal
+    // I am gonna give the User which is coming from the database to Spring by using loadUserByUsername method is coming form the UserDetailsService
     }
 }
