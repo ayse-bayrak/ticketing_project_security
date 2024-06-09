60889c6d42d968d4d2a366d0412f92476dc20b40
ayse-bayrak <aysegunes.bayrak82@gmail.com>
2024-06-09 08:40:26 +0300

    Who Login part

--- src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -99,7 +99,7 @@ public class ProjectServiceImpl implements ProjectService {
     @Override
     public List<ProjectDTO> listAllProjectDetails() {
         String username= SecurityContextHolder.getContext().getAuthentication().getName();
-        //secuity is gonna bring me the information of the user who ever is logging in the application at that moment.
+        //security is gonna bring me the information of the user who ever is logging in the application at that moment.
         // when I want to project create page as a manager Mike, I could not see and I gave source can not be null error
         //because there some information project related with manager. But earlier in here I put hardcoded log in harold@
         //UserDTO currentUserDTO = userService.findByUserName("harold@manager.com");
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -133,7 +133,7 @@ public class TaskServiceImpl implements TaskService {
     public List<TaskDTO> listAllTasksByStatus(Status status) {
         //UserDTO loggedInUser = userService.findByUserName("john@employee.com");
         String username= SecurityContextHolder.getContext().getAuthentication().getName();
-        UserDTO loggedInUser = userService.findByUserName(username );
+        UserDTO loggedInUser = userService.findByUserName(username);
         List<Task> tasks = taskRepository.
                 findAllByTaskStatusAndAssignedEmployee(status, userMapper.convertToEntity(loggedInUser));
         return tasks.stream().map(taskMapper::convertToDto).collect(Collectors.toList());
